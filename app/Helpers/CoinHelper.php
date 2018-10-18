1e8f70660c82e68e49811d8a4d745ed4e4125246
Ilyes Tascou <41997927+ilyes-plenty@users.noreply.github.com>
2018-10-18 16:23:00 +0200

    fix vars

--- app/Helpers/CoinHelper.php
+++ app/Helpers/CoinHelper.php
@@ -13,44 +13,38 @@ use GuzzleHttp\Client;
 class CoinHelper
 {
 
-    private $API_URL = 'https://api.coinmarketcap.com/v2/';
-    private $endpoint = "ticker/?limit=50";
+    private static $api_url = 'https://api.coinmarketcap.com/v2/';
+    private static $endpoint = "ticker/?limit=50";
 
     private $coinArray = array();
 
 
     function requestToCoin ()
     {
-        try {
-            // Eventuell auf ASync umstellen?
-            $client = new Client();
-            $request = $client->request('GET', $this->API_URL . $this->endpoint);
+        // Eventuell auf ASync umstellen?
+        $client = new Client();
+        $request = $client->request('GET', self::$api_url . self::$endpoint);
 
 
-            $items = json_decode($request->getBody());
+        $items = json_decode($request->getBody());
 
 
-            foreach($items->data AS $coin)
-            {
-                $tmpCoin = new Coin();
+        foreach($items->data AS $coin)
+        {
+            $tmpCoin = new Coin();
 
-                $tmpCoin->id = $coin->id;
-                $tmpCoin->name = $coin->name;
-                $tmpCoin->symbol = $coin->symbol;
-                $tmpCoin->rank = $coin->rank;
-                $tmpCoin->price = $coin->quotes->USD->price;
-                $tmpCoin->vol24h = $coin->quotes->USD->volume_24h;
-                $tmpCoin->marketcap = $coin->quotes->USD->market_cap;
-                $tmpCoin->change1h = $coin->quotes->USD->percent_change_1h;
-                $tmpCoin->change24h = $coin->quotes->USD->percent_change_24h;
-                $tmpCoin->change7d = $coin->quotes->USD->percent_change_7d;
+            $tmpCoin->id = $coin->id;
+            $tmpCoin->name = $coin->name;
+            $tmpCoin->symbol = $coin->symbol;
+            $tmpCoin->rank = $coin->rank;
+            $tmpCoin->price = $coin->quotes->USD->price;
+            $tmpCoin->vol24h = $coin->quotes->USD->volume_24h;
+            $tmpCoin->marketcap = $coin->quotes->USD->market_cap;
+            $tmpCoin->change1h = $coin->quotes->USD->percent_change_1h;
+            $tmpCoin->change24h = $coin->quotes->USD->percent_change_24h;
+            $tmpCoin->change7d = $coin->quotes->USD->percent_change_7d;
 
-                $this->coinArray[] = $tmpCoin;
-            }
-
-        }
-        catch (\Exception $e) {
-            dd($e->getMessage());
+            $this->coinArray[] = $tmpCoin;
         }
     }
 
